7b68eced37cbafad4daf20f212f13f87fca74711
thechosenginger <sewell.matthewm@gmail.com>
2021-09-10 21:57:50 -0500

    Updated Notes

--- client/src/Components/qa/Questions.jsx
+++ client/src/Components/qa/Questions.jsx
@@ -1,1 +1,13 @@
 // test point
+
+
+
+
+// axios.get('/getQuestions',{
+//   params: {
+//     display_id: this.state.display_id
+//     transaction_id: this.state.transaction_id
+//   }
+// }, (err, data) => {
+
+// })
