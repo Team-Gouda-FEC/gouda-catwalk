61a84acf374e741fc7845bc72fc6f8d3f1ed6fc8
thechosenginger <sewell.matthewm@gmail.com>
2021-09-12 20:21:35 -0500

    Added sudo code for front end axios requests

--- client/src/Components/qa/Questions.jsx
+++ client/src/Components/qa/Questions.jsx
@@ -22,8 +22,14 @@
 
 // axios.post('/addQuestion', {
 //   params: {
-//     question_id: this.state.product_id,
+//     question_id: this.state.question_id,
 //     page: this.state.page,
 //     count: this.state.count
+//   }
+// })
+
+// axios.put('/markQuestion', {
+//   params: {
+//     question_id: this.state.question_id
 //   }
 // })
