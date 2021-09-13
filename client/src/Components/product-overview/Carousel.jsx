832bf26222dd1eefb96fa72b9f99aac0f2529d06
Rebekah Cruz <rebekah.cruz702@gmail.com>
2021-09-13 18:56:28 -0500

    EOD workflow commit

---
+++ client/src/Components/product-overview/Carousel.jsx
@@ -0,0 +1,183 @@
+import React from 'react';
+import ImageGallery from 'react-image-gallery';
+
+const PREFIX_URL = 'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/';
+
+const images = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+];
+
+export default class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showIndex: false,
+      showBullets: true,
+      infinite: true,
+      showThumbnails: true,
+      showFullscreenButton: true,
+      showGalleryFullscreenButton: true,
+      showPlayButton: true,
+      showGalleryPlayButton: true,
+      showNav: true,
+      isRTL: false,
+      slideDuration: 450,
+      slideInterval: 2000,
+      slideOnThumbnailOver: false,
+      thumbnailPosition: 'bottom',
+      showVideo: {},
+      useWindowKeyDown: true,
+    };
+
+    this.images = [
+      {
+        thumbnail: `${PREFIX_URL}4v.jpg`,
+        original: `${PREFIX_URL}4v.jpg`,
+        embedUrl: 'https://www.youtube.com/embed/4pSzhZ76GdM?autoplay=1&showinfo=0',
+        description: 'Render custom slides (such as videos)',
+        renderItem: this._renderVideo.bind(this)
+      },
+      {
+        original: `${PREFIX_URL}1.jpg`,
+        thumbnail: `${PREFIX_URL}1t.jpg`,
+        originalClass: 'featured-slide',
+        thumbnailClass: 'featured-thumb',
+        description: 'Custom class for slides & thumbnails',
+      },
+    ].concat(this._getStaticImages());
+  }
+
+  _onImageClick(event) {
+    console.debug('clicked on image', event.target, 'at index', this._imageGallery.getCurrentIndex());
+  }
+
+  _onImageLoad(event) {
+    console.debug('loaded image', event.target.src);
+  }
+
+  _onSlide(index) {
+    this._resetVideo();
+    console.debug('slid to index', index);
+  }
+
+  _onPause(index) {
+    console.debug('paused on index', index);
+  }
+
+  _onScreenChange(fullScreenElement) {
+    console.debug('isFullScreen?', !!fullScreenElement);
+  }
+
+  _onPlay(index) {
+    console.debug('playing from index', index);
+  }
+
+  _handleInputChange(state, event) {
+    if (event.target.value > 0) {
+      this.setState({ [state]: event.target.value });
+    }
+  }
+
+  _handleCheckboxChange(state, event) {
+    this.setState({ [state]: event.target.checked });
+  }
+
+  _handleThumbnailPositionChange(event) {
+    this.setState({ thumbnailPosition: event.target.value });
+  }
+
+  _getStaticImages() {
+    let images = [];
+    for (let i = 2; i < 12; i++) {
+      images.push({
+        original: `${PREFIX_URL}${i}.jpg`,
+        thumbnail: `${PREFIX_URL}${i}t.jpg`
+      });
+    }
+
+    return images;
+  }
+
+  _resetVideo() {
+    this.setState({ showVideo: {} });
+
+    if (this.state.showPlayButton) {
+      this.setState({ showGalleryPlayButton: true });
+    }
+
+    if (this.state.showFullscreenButton) {
+      this.setState({ showGalleryFullscreenButton: true });
+    }
+  }
+
+  _toggleShowVideo(url) {
+    this.state.showVideo[url] = !Boolean(this.state.showVideo[url]);
+    this.setState({
+      showVideo: this.state.showVideo
+    });
+
+    if (this.state.showVideo[url]) {
+      if (this.state.showPlayButton) {
+        this.setState({ showGalleryPlayButton: false });
+      }
+
+      if (this.state.showFullscreenButton) {
+        this.setState({ showGalleryFullscreenButton: false });
+      }
+    }
+  }
+
+  _renderVideo(item) {
+    return (
+      <div>
+        {
+          this.state.showVideo[item.embedUrl] ?
+            <div className='video-wrapper'>
+              <a
+                className='close-video'
+                onClick={this._toggleShowVideo.bind(this, item.embedUrl)}
+              >
+              </a>
+              <iframe
+                width='560'
+                height='315'
+                src={item.embedUrl}
+                frameBorder='0'
+                allowFullScreen
+              >
+              </iframe>
+            </div>
+            :
+            <a onClick={this._toggleShowVideo.bind(this, item.embedUrl)}>
+              <div className='play-button'></div>
+              <img className='image-gallery-image' src={item.original} />
+              {
+                item.description &&
+                <span
+                  className='image-gallery-description'
+                  style={{ right: '0', left: 'initial' }}
+                >
+                  {item.description}
+                </span>
+              }
+            </a>
+        }
+      </div>
+    );
+  }
+
+  render() {
+    return <ImageGallery items={images} />;
+  }
+}
--- client/src/Components/product-overview/GridContainer.jsx
+++ client/src/Components/product-overview/GridContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-// import Carousel from './Carousel';
+import Carousel from './Carousel.jsx';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -26,7 +26,7 @@ export default function GridContainer () {
     <div classname = {classes.root}>
       <Grid container elevation={0}>
         <Grid item xs={7} >
-        {/* <Carousel /> */}
+        <Carousel />
         </Grid>
         <Grid item xs={5} >
         <Paper className={classes.paper}> this is where the interactive category info will go! </Paper>
