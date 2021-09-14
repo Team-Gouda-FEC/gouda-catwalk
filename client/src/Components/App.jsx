17aee0635e18af04b43cd3d62015a6bafb07427b
Rebekah Cruz <rebekah.cruz702@gmail.com>
2021-09-14 11:50:23 -0500

    update code to meet linter requirements

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import axios from 'axios';
+import Axios from 'axios';
 import NavBar from './product-overview/NavBar.jsx';
 import GridContainer from './product-overview/GridContainer.jsx';
-import Stars from './starRating/starRating.jsx';
+import Stars from './rating-review/StarRating';
 import RelatedProductCard from './related-items-section/relatedProductCard.jsx';
 import Carousel from './carousel/carousel.jsx';
 
@@ -19,26 +19,38 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div className="App" style={{ maxWidth: 1200, marginLeft: 'auto', marginRight: 'auto', marginTop: 64 }}>
-        <NavBar />
-        <h3> there is definitely some sort of announcement here! </h3>
-        <GridContainer />
-        This is our homepage now :D and Maria is the Goudest
-        <Stars />
-        <Carousel show={4}>
-          <div>
-            <div style={{padding: 8}}>
-              <img src="https://via.placeholder.com/300x300" alt="placeholder" style={{width: '100%'}} />
+      <>
+        <div
+          className="App"
+          style={{
+            maxWidth: 1200,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            marginTop: 64,
+          }}
+        >
+          <NavBar />
+          <h2> there is definitely some sort of announcement here! </h2>
+          <GridContainer />
+          This is our homepage now :D and Maria is the Goudest
+          <Stars />
+          <Carousel show={4}>
+            <div>
+              <div style={{
+                padding: 8,
+              }}
+              >
+                <img src="https://via.placeholder.com/300x300" alt="placeholder" style={{width: '100%'}} />
+              </div>
             </div>
-          </div>
-          <RelatedProductCard />
-          <RelatedProductCard />
-          <RelatedProductCard />
-          <RelatedProductCard />
-          <RelatedProductCard />
-        </Carousel>
-
-      </div>
+            <RelatedProductCard />
+            <RelatedProductCard />
+            <RelatedProductCard />
+            <RelatedProductCard />
+            <RelatedProductCard />
+          </Carousel>
+        </div>
+      </>
     );
   }
 }
--- client/src/Components/product-overview/NavBar.jsx
+++ client/src/Components/product-overview/NavBar.jsx
@@ -88,5 +88,5 @@ export default function NavBar() {
         </Toolbar>
       </AppBar>
     </div>
-  )
-};
+  );
+}
