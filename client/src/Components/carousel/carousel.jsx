1a6d9b6fe7f9e87373792739a82cbfb623c91885
mariaykim <jsmkim09@gmail.com>
2021-09-14 09:18:13 -0500

    current progress on carousel

--- client/src/Components/carousel/carousel.jsx
+++ client/src/Components/carousel/carousel.jsx
@@ -1,34 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './carousel.css';
 
-
-
 const Carousel = (props) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-const [length, setLength] = useState(children.length);
-  useEffect(() => {
-    setLength(children.length)
-}, [children])
-
   const { children } = props;
 
-    return (
-        <div className="carousel-container">
-            <div className="carousel-wrapper">
-              {/* You can alwas change the content of the button to other things */}
-              <button className="left-arrow">
-                  &lt;
-              </button>
-              <div className="carousel-content-wrapper">
-                  { /*...*/ }
-              </div>
-              {/* You can alwas change the content of the button to other things */}
-              <button className="right-arrow">
-                  &gt;
-              </button>
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [length, setLength] = useState(children.length);
+  useEffect(() => {
+    setLength(children.length);
+  }, [children]);
+
+  const next = () => {
+    if (currentIndex < (length - 1)) {
+      setCurrentIndex((prevState) => prevState + 1);
+    }
+  };
+
+  const prev = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex((prevState) => prevState - 1);
+    }
+  };
+
+  return (
+    <div className="carousel-container">
+      <div className="carousel-wrapper">
+        {
+          currentIndex > 0 && <button onClick={prev} className="left-arrow"> &lt; </button>
+        }
+        <div className="carousel-content-wrapper">
+          <div className="carousel-content" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+            {children}
           </div>
         </div>
-    )
-}
+        {
+          currentIndex < (length - 1) && <button onClick={next} className="right-arrow"> &gt; </button>
+        }
+      </div>
+    </div>
+  );
+};
 
-export default Carousel
+export default Carousel;
